b3ce5f5af3e71980ae7d493334eb6d7a4bd3aad8
lzz <972321170@qq.com>
2025-10-27 21:45:37 +0800

    程序调用AI大模型 - Spring AI

---
+++ src/main/java/com/example/myagent/demo/invoke/SpringAiAiInvoke.java
@@ -0,0 +1,23 @@
+package com.example.myagent.demo.invoke;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringAiAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = dashscopeChatModel.call(new Prompt("hello, I'm lzz"))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}
