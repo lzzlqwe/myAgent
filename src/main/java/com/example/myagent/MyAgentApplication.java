4efef9307f7064336183f21ea5daf6d903a86f5a
lzz <972321170@qq.com>
2025-10-27 20:18:02 +0800

    项目初始化

---
+++ src/main/java/com/example/myagent/MyAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.myagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAgentApplication.class, args);
+    }
+
+}
