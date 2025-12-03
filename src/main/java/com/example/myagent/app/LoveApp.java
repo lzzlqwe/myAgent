306e56512a52ea601ec21e90d5160a324fa7819e
lzz <972321170@qq.com>
2025-12-03 16:04:43 +0800

    3.AI应用开发 - 扩展知识 - 自定义日志 Advisor

--- src/main/java/com/example/myagent/app/LoveApp.java
+++ src/main/java/com/example/myagent/app/LoveApp.java
@@ -1,5 +1,6 @@
 package com.example.myagent.app;
 
+import com.example.myagent.advisor.MyLoggerAdvisor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -33,7 +34,8 @@ public class LoveApp {
         chatClient = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory)
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new MyLoggerAdvisor()
                 )
                 .build();
     }
